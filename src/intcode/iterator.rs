4d3ee6877636d8197bea432d2f5a9d045646bd7b
Eric Mertens <emertens@gmail.com>
2021-07-25 19:48:04 -0700

    simplify machine iterator

--- src/intcode/iterator.rs
+++ src/intcode/iterator.rs
@@ -1,46 +1,26 @@
 use super::{Machine, Step};
 
-#[derive(Copy, Clone, Debug)]
-enum State {
-    Ready,
-    Halted,
-    Crashed,
-}
-
 pub struct MachineIterator<I> {
     machine: Machine,
-    state: State,
     inputs: I,
 }
 
 impl<I: Iterator<Item = i64>> Iterator for MachineIterator<I> {
     type Item = i64;
     fn next(&mut self) -> Option<Self::Item> {
-        match self.state {
-            State::Ready => loop {
-                match self.machine.step() {
-                    Err(_) => {
-                        self.state = State::Crashed;
-                        return None;
+        loop {
+            match self.machine.step() {                    
+                Ok(Step::Output(o)) => return Some(o),
+                Ok(Step::Input(i)) => match self.inputs.next() {
+                    Some(x) => self.machine[i] = x,
+                    None => {
+                        return None
                     }
-                    Ok(Step::Halt) => {
-                        self.state = State::Halted;
-                        return None;
-                    }
-                    Ok(Step::Output(o)) => {
-                        self.state = State::Ready;
-                        return Some(o);
-                    }
-                    Ok(Step::Input(i)) => match self.inputs.next() {
-                        None => {
-                            self.state = State::Crashed;
-                            return None;
-                        }
-                        Some(x) => self.machine[i] = x,
-                    },
+                },
+                _ => {
+                    return None
                 }
-            },
-            _ => None,
+            }
         }
     }
 }
@@ -52,7 +32,6 @@ pub trait MachineIteratorExt: Iterator {
     {
         MachineIterator {
             machine: Machine::new(pgm),
-            state: State::Ready,
             inputs: self,
         }
     }
